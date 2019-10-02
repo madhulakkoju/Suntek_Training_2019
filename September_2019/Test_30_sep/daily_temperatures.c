fa8ae80736c0ef23e06c0cce61193616cd4f9599
Madhu Lakkoju <53974577+madhulakkoju@users.noreply.github.com>
2019-10-02 14:51:56 +0530

    Update daily_temperatures.c

--- September_2019/Test_30_sep/daily_temperatures.c
+++ September_2019/Test_30_sep/daily_temperatures.c
@@ -1,14 +1,19 @@
+/*
+A list of daily temperatures T , return a list such that for each day in the list , tells you how many days you would have to wait until a warmer temperature.
+If there is no futire day in which this is possible, put 0 instead.
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(int argc,char* argv)
+int main()
 {
-  int n = atoi(argv[1]);
+  int n;
+  scanf("%d",&n);
   int *a = (int*)malloc(n*sizeof(int));
   for(int i=0;i<n;i++)
   {
-  a[i] = atoi(argv[i+2]);
+  scanf("%d",&a[i]);
   }
   int i=0,j=0,c=0;
   for(i=0;i<n;i++)
@@ -29,3 +34,12 @@ printf("%d ",c);
 }
 return 0;
 }
+
+/*
+
+INPUT 
+8                                                                
+73 74 75 71 69 72 76 73 
+OUTPUT
+1 1 4 2 1 1 0 0       
+*/
