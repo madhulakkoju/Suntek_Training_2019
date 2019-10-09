22f4512bcf8ad17d96dbeb63a00a304b540c9956
Madhu Lakkoju <53974577+madhulakkoju@users.noreply.github.com>
2019-10-09 20:31:59 +0530

    Create jsonParse.c

---
+++ October_2019/Microsoft_Internship_Exam/jsonParse.c
@@ -0,0 +1,27 @@
+
+int jsonParse(char** input1)
+{
+  int max =0,push =0;
+  for(i=0;i<strlen(input1);i++)
+  {
+  if(input1[i]=='['||input1[i]=='{'||input1[i]=='(')
+  {
+  push ++;
+  if(max<push) max = push;
+  }
+  else if(input1[i]==']'||input1[i]=='}'||input1[i]==')')
+  {
+  push--;
+  }
+  }
+  return max;
+}
+
+/*
+INPUT / OUTPUT
+
+{[[()]]}
+
+4
+
+*/
