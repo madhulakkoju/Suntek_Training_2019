8fef06af496f8ea369076885b4bb6536c8ad6786
Madhu Lakkoju <53974577+madhulakkoju@users.noreply.github.com>
2019-10-09 20:10:59 +0530

    Update Remove_duplicates.c

--- October_2019/Microsoft_Internship_Exam/Remove_duplicates.c
+++ October_2019/Microsoft_Internship_Exam/Remove_duplicates.c
@@ -1,1 +1,23 @@
 
+char *removeDuplicates(char *input1)
+{
+ int i =0,j=0,m=0,tt=0;
+  int n =strlen(input1);
+  char c;
+  char *ch =(char *)malloc(sizeof(char)*n);
+  while(i<n)
+  {
+   tt=0;j=0;
+    while(j<m)
+    {
+      if(input1[i]==ch[j])
+      tt=1;
+      j++;
+    }
+    if(tt=0)
+      ch[m++]=input1[i];
+    i++;
+  }
+  ch[m]='\0';
+  return ch;
+}
